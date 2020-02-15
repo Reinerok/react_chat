822ec61200dde4c0809dd44eec002b064291447d
Reinerok <reinerok@gmail.com>
2020-02-15 09:10:50 +0500

    подготовка

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="chat"></div>
+      <input type="text" className="input-line"/>
+    </div>
+  );
+}
+
+export default App;
