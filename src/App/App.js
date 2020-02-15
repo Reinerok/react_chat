75bf28239847ca38d34e79a23caaefe69da7b4af
Reinerok <reinerok@gmail.com>
2020-02-15 18:11:50 +0500

    обновление 1

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="chat"></div>
-      <input type="text" className="input-line"/>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    value:'',
+    valueChat:'',
+    timer: false
+  }
+
+  typeTextHandler = (e) => {
+    this.setState({
+      valueChat:this.state.valueChat + this.state.value
+    })
+  }
+
+  changeTextHandler = (e) =>{
+    this.setState({
+      value:e.target.value
+    })
+  }
+
+  changeText = (stop) => {
+   /* let test = setInterval(()=>{
+      this.setState({
+        valueChat:this.state.valueChat + 1,
+        timer:true
+      })
+    },10000)*/
+  }
+
+  render() {
+    console.log(1)
+    if(!this.state.timer){
+      this.changeText();
+    }
+
+    return (
+
+
+
+      <div className="App">
+        <div className="chat">{this.state.valueChat}</div>
+        <Input value={this.state.value} onChange={this.changeTextHandler}/>
+        <input type="submit" className="input-submit" onClick={this.typeTextHandler}/>
+      </div>
+    );
+  }
+}
+
+function Input(props) {
+  return <input type="text" value={props.value} className="input-line" onChange={props.onChange}/>
 }
 
 export default App;
